cec1b5b1b076868e8ed3a9f26373751900e1dfdb
Nour-AlSulais <s202271740@kfupm.edu.sa>
2026-09-01 11:28:40 +0300

    first commit

---
+++ myapp2/lib/main.dart
@@ -0,0 +1,21 @@
+// void main () {
+//   print('Hi my name is Nour');
+// }
+
+//Task 1.1
+
+void main() {
+  int age = 22;
+  double height = 1.57;
+  bool isStudent = true;
+  String name = 'Nour';
+  List<int> scores = [100, 100, 100];
+
+  print('$name is $age years old and $height meters tall.');
+  print('Student: $isStudent');
+  print('Scores: $scores');
+
+  	var city = 'Qatif';
+	print('City: $city');
+}
+
