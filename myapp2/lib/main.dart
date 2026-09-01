99dea187ee4e8329665a1642df8506f470710b4a
Nour-AlSulais <s202271740@kfupm.edu.sa>
2026-09-01 11:38:34 +0300

    task 2

--- myapp2/lib/main.dart
+++ myapp2/lib/main.dart
@@ -4,18 +4,41 @@
 
 //Task 1.1
 
+// void main() {
+//   int age = 22;
+//   double height = 1.57;
+//   bool isStudent = true;
+//   String name = 'Nour';
+//   List<int> scores = [100, 100, 100];
+
+//   print('$name is $age years old and $height meters tall.');
+//   print('Student: $isStudent');
+//   print('Scores: $scores');
+
+//   	var city = 'Qatif';
+// 	print('City: $city');
+// }
+
+//task 2 
+
 void main() {
-  int age = 22;
-  double height = 1.57;
-  bool isStudent = true;
-  String name = 'Nour';
-  List<int> scores = [100, 100, 100];
+  final String courseName = 'Mobile App Development';
+  const double pi = 3.14159;
 
-  print('$name is $age years old and $height meters tall.');
-  print('Student: $isStudent');
-  print('Scores: $scores');
+  dynamic anything = 'I can be any type!';
+// courseName = 'Web Development';
+// pi = 3.14;
+  print(courseName);
+  print(pi);
+  print(anything);
 
-  	var city = 'Qatif';
-	print('City: $city');
+  anything = 42;
+  print(anything);
 }
+
+// 1. What is the difference between `final` and `const`?
+// final Set once at runtime and const set at compile time.
+//2. Why can `dynamic` change from `String` to `int`?
+//turns off compile-time type checking.
+
 
